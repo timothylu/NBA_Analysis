46bbf4a1d2326489994c26d68fbf870118730e28
timothylu <timothy.e.lu@gmail.com>
2018-03-18 16:18:20 -0700

    plots added

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 import goldsberry
 import pandas as pd
+import matplotlib.pyplot as plt
 
 players = goldsberry.PlayerList(Season='2017-18')
 players2017 = pd.DataFrame(players.players())
@@ -10,4 +11,10 @@ lillard_id = players2017.loc[players2017['PLAYERCODE'] == 'damian_lillard']['PER
 lillard_game_logs = goldsberry.player.game_logs(lillard_id)
 lillard_game_logs_2017 = pd.DataFrame(lillard_game_logs.logs())
 
-print lillard_game_logs_2017
+lgl17 = lillard_game_logs_2017.loc[(lillard_game_logs_2017['PTS'] > 25) & (lillard_game_logs_2017['PTS'] < 30)]
+
+ts = pd.DataFrame(lgl17, columns=['FGA'])
+print ts.mean()
+
+ts = ts.plot()
+plt.show()
