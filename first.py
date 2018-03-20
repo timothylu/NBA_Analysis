a69674267b36d79387fad1890b6f1d803dd78831
timothylu <timothy.e.lu@gmail.com>
2018-03-20 09:37:48 -0700

    post all-star break stats (dataframe by row)

--- first.py
+++ first.py
@@ -10,10 +10,15 @@ lillard_id = players2017.loc[players2017['PLAYERCODE'] == 'damian_lillard']['PER
 
 lillard_game_logs = goldsberry.player.game_logs(lillard_id)
 lillard_game_logs_2017 = pd.DataFrame(lillard_game_logs.logs())
+post_all_star = lillard_game_logs_2017.loc[58:82]
+#lgl17 = post_all_star.loc[
+#								(post_all_star['PTS'] > 25)
+#								& (post_all_star['PTS'] < 30)
+#								]
 
-lgl17 = lillard_game_logs_2017.loc[(lillard_game_logs_2017['PTS'] > 25) & (lillard_game_logs_2017['PTS'] < 30)]
-
-ts = pd.DataFrame(lgl17, columns=['FGA'])
+ts = pd.DataFrame(lgl17, columns=['PTS'])
+print ts
+print "\n"
 print ts.mean()
 
 ts = ts.plot()
