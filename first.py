c5000b0946a6a206805bfaacbec318932d75120b
timothylu <timothy.e.lu@gmail.com>
2018-03-20 13:36:32 -0700

    plot lines on top of each other, seperated pre and post all-star break

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 import goldsberry
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 players = goldsberry.PlayerList(Season='2017-18')
@@ -11,15 +12,19 @@ lillard_id = players2017.loc[players2017['PLAYERCODE'] == 'damian_lillard']['PER
 lillard_game_logs = goldsberry.player.game_logs(lillard_id)
 lillard_game_logs_2017 = pd.DataFrame(lillard_game_logs.logs())
 post_all_star = lillard_game_logs_2017.loc[58:82]
+pre_all_star = lillard_game_logs_2017.loc[0:57]
 #lgl17 = post_all_star.loc[
 #								(post_all_star['PTS'] > 25)
 #								& (post_all_star['PTS'] < 30)
 #								]
 
-ts = pd.DataFrame(lgl17, columns=['PTS'])
-print ts
+after = np.array(pd.DataFrame(post_all_star, columns=['PTS']))
+before = np.array(pd.DataFrame(pre_all_star, columns=['PTS']))
+print after.mean()
 print "\n"
-print ts.mean()
+print before.mean()
 
-ts = ts.plot()
+scaled_post = (pd.Series(range(1,len(after) + 1)) * int(round(len(before)/len(after)))).tolist()
+
+plt.plot(scaled_post,after,list(range(1, len(before) + 1)), before)
 plt.show()
